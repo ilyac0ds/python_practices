2cbfe9e2dbf3a2258b5b5f638a15145af2725ef3
Ilya <laww0514@gmail.com>
2026-08-09 03:00:57 +0330

    task4.py

--- task4.py
+++ task4.py
@@ -1,17 +1,19 @@
-n = int(input("enter number : "))
+def sam():
+    n = int(input("enter number : "))
+    numbers = []
+    
+    for i in range(n):
+        number = float(input(f"enter number {i} : "))
+        numbers.append(number)
 
-numbers = []
+    total = sum(numbers)
+    maximum = max(numbers)
+    minimum = min(numbers)
+    average = total / n
+    
+    print(f"sum is {total}")
+    print(f"average is {average}")
+    print(f"maximum is {maximum}")
+    print(f"minimum is {minimum}")
 
-for i in range(n):
-    number = float(input(f"enter number {i} : "))
-    numbers.append(number)
-
-total = sum(numbers)
-maximum = max(numbers)
-minimum = min(numbers)
-average = total / n
-
-print(f"sum is {total}")
-print(f"average is {average}")
-print(f"maximum is {maximum}")
-print(f"minimum is {minimum}")
+sam()
