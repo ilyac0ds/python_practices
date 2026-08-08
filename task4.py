61682761d37c49e364364aabd3258908e7d8058b
Ilya <laww0514@gmail.com>
2026-08-08 21:50:11 +0330

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,17 @@
+n = int(input("enter number : "))
+
+numbers = []
+
+for i in range(n):
+    number = float(input(f"enter number {i} : "))
+    numbers.append(number)
+
+total = sum(numbers)
+maximum = max(numbers)
+minimum = min(numbers)
+average = total / n
+
+print(f"sum is {total}")
+print(f"average is {average}")
+print(f"maximum is {maximum}")
+print(f"minimum is {minimum}")
